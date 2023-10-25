67685d7a7879e0601a4f4809933129e126e97112
Riya Bisht <79960426+chococandy63@users.noreply.github.com>
2023-10-25 18:08:39 +0530

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -22,6 +22,7 @@
     # # "Hello, world!" when run.
     # pkgs.hello
     duf
+    brightnessctl
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
